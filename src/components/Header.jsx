50a97a687c4a8d99b18fcd916b7088b349460bb7
Edwin <edwinleal019@gmail.com>
2021-10-10 20:56:48 +0200

    feat(innovamat): toggle to nav bar header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
 import { Link } from "react-router-dom";
-import { appHeader, navButton } from "./Header.module.css";
+import { appHeader, navButton, activeButton } from "./Header.module.css";
+import { TALLERES_PATH, RINCONES_PATH } from "../routing/paths";
+import { useState } from "react";
 
 export default function Header() {
+  const [active, setActive] = useState(true);
+  const handleToggle = () => setActive(!active);
+  const NORMAL_BUTTON = navButton;
+  const ACTIVE_BUTTON = `${navButton} ${activeButton}`;
+
   return (
     <header className={appHeader}>
       <nav>
-        <button className={navButton}>
-          <Link to="/">Talleres</Link>
-        </button>
-        <button className={navButton}>
-          <Link to="/rincones">Rincones</Link>
-        </button>
+        <Link to={TALLERES_PATH}>
+          <button
+            className={active ? `${ACTIVE_BUTTON}` : `${NORMAL_BUTTON}`}
+            onClick={handleToggle}
+          >
+            Talleres
+          </button>
+        </Link>
+        <Link to={RINCONES_PATH}>
+          <button
+            className={active ? `${NORMAL_BUTTON}` : `${ACTIVE_BUTTON}`}
+            onClick={handleToggle}
+          >
+            Rincones
+          </button>
+        </Link>
       </nav>
     </header>
   );
