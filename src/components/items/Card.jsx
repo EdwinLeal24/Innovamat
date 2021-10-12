cc0601ecf664127899202297bd8434a7bd07a52a
Edwin <edwinleal019@gmail.com>
2021-10-12 23:21:13 +0200

    bug(innovamat): add favorite button fix

--- src/components/items/Card.jsx
+++ src/components/items/Card.jsx
@@ -9,30 +9,40 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { IoIosHeartEmpty, IoIosHeart } from "react-icons/io";
 import { useLocation } from "react-router-dom";
+import { DETAIL_PATH } from "../../routing/paths";
 
 export default function Card({ source, changeFavorite }) {
   const { id, image, tag, title, description } = source;
 
   const [isFavorite, setIsFavorite] = useState(false);
+  const [isHover, setIsHover] = useState(false);
   const addFavorite = () => {
     let favorite = isFavorite;
     favorite = !favorite;
     setIsFavorite(favorite);
     changeFavorite(source, favorite);
   };
+
   const location = useLocation();
   const showButton = location.pathname.includes("favoritos");
   const colorButton = { color: isFavorite ? "#18c6ca" : "#6c6c6c" };
+
+  const showFavorite = isFavorite || isHover;
+
   return (
-    <li className={card}>
-      <Link to={`/resource/${id}`}>
+    <li
+      className={card}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <Link to={`${DETAIL_PATH}/${id}`}>
         <div>
           <img src={image} alt={("image", tag)} className={cardImage} />
           <h4 className={cardTitle}>{title}</h4>
           <p className={cardDescription}>{description}</p>
         </div>
       </Link>
-      {!showButton && (
+      {showFavorite && (
         <div>
           <button onClick={addFavorite}>
             <p className={favoriteCard} style={colorButton}>
