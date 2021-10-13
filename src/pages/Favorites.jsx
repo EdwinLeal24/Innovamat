029c238cc92297410ae634a7edbccd91237a4a03
Edwin <edwinleal019@gmail.com>
2021-10-13 23:52:53 +0200

    feat(innovamat): user can remove favorite item

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -4,25 +4,26 @@ import { FaArrowLeft } from "react-icons/fa";
 import { TALLERES_PATH } from "../routing/paths";
 import { buttonBack } from "./Favorite.module.css";
 import GridCards from "../components/items/GridCards";
-import Dialog from "../components/Dialog";
+import Dialog from "../components/dialog/Dialog";
 
 export default function Favorites() {
   const history = useHistory();
   const favorites = getFavorites();
   const message = "Aún no has añadido recursos a tus favoritos ¿añadimos?";
+
   return (
-    <>
+    <main>
       <h1>
         <button onClick={() => history.goBack()} className={buttonBack}>
           <FaArrowLeft />
         </button>
         <span>Favoritos</span>
       </h1>
-      {favorites ? (
+      {favorites.length ? (
         <GridCards resources={favorites} />
       ) : (
         <Dialog message={message} link={TALLERES_PATH} />
       )}
-    </>
+    </main>
   );
 }
